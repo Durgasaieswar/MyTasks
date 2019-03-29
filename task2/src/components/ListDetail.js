bf107308b9597d30ea78c0704be445c6337b2ce5
durgasaieswar <durgasaieswar@gmail.com>
2019-03-29 14:36:15 +0530

    Updated

--- task2/src/components/ListDetail.js
+++ task2/src/components/ListDetail.js
@@ -5,7 +5,7 @@ class subPart2 extends React.Component {
             <div className="active teal item">
                 <b>{this.props.Each}</b>
                 <div className="ui label">
-                    <button onClick={() => this.onSelecti(this.props.Each)}><i className="pencil alternate icon"></i></button>
+                    <button onClick={() => this.props.onSelecti(this.props.Each)}><i className="pencil alternate icon"></i></button>
                     <button onClick={() => this.props.onSelect(this.props.Each)}><i className="trash alternate icon"></i></button>
                 </div>
             </div>
